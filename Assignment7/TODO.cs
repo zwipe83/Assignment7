56e17d33b36ef53b5f09debc0d574a71d6f0254a
Samuel J <161459858+zwipe83@users.noreply.github.com>
2024-05-11 23:10:32 +0200

    Update TODO.cs

--- Assignment7/TODO.cs
+++ Assignment7/TODO.cs
@@ -1,10 +1,10 @@
-﻿//TODO: Add description field for Animal class
-//TODO: Serialize and persist AnimalManager.ListOfAnimals.
-//TODO: Add images to listview?
+﻿//FIXED: Add description field for Animal class
+//FIXED: Serialize and persist AnimalManager.ListOfAnimals.
+//TODO: Add images to listview? Maybe not.
 //TODO: Add an Image class?
 //TODO: If I change an animal name, the observablecollection name isn't updated
 //TODO: Set max limit to observablecollections?
-//TODO: Add method to adjust properties in Sightings collection if some of the underlying properties have changed. For example Animal.Name
+//TODO: Add method to update properties in Sightings collection if some of the underlying properties have changed. For example Animal.Name
 //TODO: Go over access modifiers
 //TODO: Add possibility to add image to sighting
 //TODO: Maybe combine Date and Time classes to one, DateTime?
@@ -14,3 +14,4 @@
 //TODO: Prettify AnimalWindow
 //TODO: Implement printing functions
 //TODO: Implement menu functions
+//TODO: Add description field to Sighting.
